6fa01cf426e4af4b45cc6d2128537a96976db4de
Ouedraogo ousmane <generaloks496@gmail.com>
2022-09-29 14:10:12 +0000

    settings updated

--- logistic/settings.py
+++ logistic/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+import django_heroku
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +30,7 @@ SECRET_KEY = "django-insecure-m&cgke#%w$-q0-$k#=4jj--gqget_l)-3))2pzf+!t70cw-=g5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com','https://logistic-backend2022.herokuapp.com/']
 
 
 # Application definition
@@ -148,6 +153,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+# Activation of django-heroku
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
